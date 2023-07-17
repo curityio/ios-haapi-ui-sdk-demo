5ce650435a4909c45d6ba7d22609baddd9cedca9
gary.archer <gary.archer@WORKMAC.local>
2023-07-17 17:23:15 +0200

    Error handling updates

--- src/Configuration.swift
+++ src/Configuration.swift
@@ -7,5 +7,5 @@ struct Configuration {
     static let baseURL = URL(string: "http://localhost:8443")!
     static let tokenEndpointURL = URL(string: "/oauth/v2/oauth-token", relativeTo: baseURL)!
     static let authorizationEndpointURL = URL(string: "/oauth/v2/oauth-authorize", relativeTo: baseURL)!
-    static let userInfoEndpointURL = URL(string: "/oauth/v2/oauth-userinfoxxx", relativeTo: baseURL)!
+    static let userInfoEndpointURL = URL(string: "/oauth/v2/oauth-userinfo", relativeTo: baseURL)!
 }
