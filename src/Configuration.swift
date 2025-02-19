699b55f142dbd9d9d5a28a00334005fcbd697c19
gary.archer <gary.archer@curity.io>
2025-02-19 19:09:11 +0100

    Updates to align with React native

--- src/Configuration.swift
+++ src/Configuration.swift
@@ -2,7 +2,7 @@ import Foundation
 
 struct Configuration {
     static let clientId = "haapi-ios-dev-client"
-    static let redirectUri = "haapi:start"
+    static let redirectUri = "haapi://"
     static let scopes = ["openid", "profile"]
     static let baseURL = URL(string: "https://localhost:8443")!
     static let tokenEndpointURL = URL(string: "/oauth/v2/oauth-token", relativeTo: baseURL)!
